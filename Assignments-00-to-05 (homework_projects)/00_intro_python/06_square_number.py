849eb35ad17751c9441a3345f7b2b9b5636d14fc
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2025-04-14 19:50:16 +0500

    Delete Assignments-00-to-05 (homework_projects)/00_intro_python directory

--- Assignments-00-to-05 (homework_projects)/00_intro_python/06_square_number.py
+++
@@ -1,8 +0,0 @@
-def main():
-    n= int(input("Enter a Number: "))
-    print("Square of Entered Number is ", n**2)
-
-# There is no need to edit code beyond this point
-
-if __name__ == '__main__':
-    main()
